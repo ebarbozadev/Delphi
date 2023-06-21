ad33316be696bd2b6c11f22e2154f1e4ca40e784
ebarbozadev <e.barboza@edu.unipar.br>
2023-06-21 11:29:01 -0300

    Adicionando atividades

---
+++ Aulas/Aula 18/Unit1.dfm
@@ -0,0 +1,30 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Impress'#227'o'
+  ClientHeight = 46
+  ClientWidth = 212
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  FormStyle = fsStayOnTop
+  Position = poScreenCenter
+  TextHeight = 15
+  object Button1: TButton
+    Left = 0
+    Top = 0
+    Width = 212
+    Height = 46
+    Align = alClient
+    Caption = 'Imprimir'
+    TabOrder = 0
+    OnClick = Button1Click
+    ExplicitLeft = 272
+    ExplicitTop = 208
+    ExplicitWidth = 75
+    ExplicitHeight = 25
+  end
+end
---
+++ Aulas/Aula 18/Unit1.pas
@@ -0,0 +1,42 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  RetornoYes: Integer;
+  RetornoNo: Integer;
+  RetornoCancel: Integer;
+begin
+  case MessageDlg('Deseja imprimir?', TMsgDlgType.mtInformation, [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo, TMsgDlgBtn.mbCancel], 0) of
+    6: ShowMessage('Imprimindo');
+    7: ShowMessage('Cancelando Impressão');
+    2: ShowMessage('Atividade Cancelada');
+  end;
+//  ShowMessage(IntToStr(RetornoYes)); // 6
+//  ShowMessage(IntToStr(RetornoNo)); // 7
+//  ShowMessage(IntToStr(RetornoCancel)); // 2
+end;
+
+end.
